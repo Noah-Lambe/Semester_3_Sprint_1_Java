5a7de7c5dfb9ee5a7c1a86c0756bab000009f37f
Hunter Saunders <Hunter.Saunders@keyin.com>
2025-02-27 10:49:00 -0330

    Files with options 16-17 on it for the menu
    In this file i have added the menu options 16-17 generate reports for expiredmeds and a list of prescriptions for a specific doctor also in the menu before the while loop an expired med to show that option 16 works and added a public arraylist for medication and prescription to medicationtracking and all files are inside src/main and all compiled to have the original .java file and the compiled .class file

---
+++ Semester_3_Sprint_1_Java/src/main/Doctor.java
@@ -0,0 +1,139 @@
+package main;
+
+public class Doctor extends Person {
+
+    private String specialization;
+    private String officeAddress;
+    private String licenseNum;
+    private String email;
+
+    // Constructor
+    public Doctor(int id,
+            String firstName,
+            String lastName,
+            int age,
+            String phoneNum,
+            String specialization,
+            String officeAddress,
+            String licenseNum,
+            String email) {
+
+        super(id, firstName, lastName, age, phoneNum);
+        this.specialization = specialization;
+        this.officeAddress = officeAddress;
+        this.licenseNum = licenseNum;
+        this.email = email;
+    }
+
+    // Getter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public String getOfficeAddress() {
+        return officeAddress;
+    }
+
+    public String licenseNum() {
+        return licenseNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter methods
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setOfficeAddress(String officeAddress) {
+        this.officeAddress = officeAddress;
+    }
+
+    public void setLicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Validation methods
+
+    public boolean isValidPhoneNum() {
+        return phoneNum.matches("\\d{3}-\\d{3}-\\d{4}"); // Used ChatGPT to help with regex statements
+    }
+
+    public boolean isValidEmail() {
+        return email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    }
+
+    public boolean isValidLicenseNum() {
+        return licenseNum.matches("^[A-Z]{3}\\d{3}$");
+    }
+
+    // To String method
+
+    @Override
+    public String toString() {
+        return "Doctor [Full Name: " + firstName + " " + lastName +
+                ", Age: " + age +
+                ", Phone Number: " + phoneNum +
+                ", Specialization: " + specialization +
+                ", Office Address: " + officeAddress +
+                ", License Number: " + licenseNum +
+                ", Email: " + email + "]";
+    }
+}
